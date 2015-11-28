65de881b3f12e24eba2932d62485029f6248dca5
earllee <earlvlee@gmail.com>
2015-11-28 12:48:18 -0500

    Fix Ember, add workout route + template.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('teams');
+  this.route('workouts');
 });
 
 export default Router;
--- app/routes/teams.js
+++ app/routes/teams.js
@@ -1,17 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
   // Get model for this route
   model: function() {
-    console.log('loading model..');
-    return this.store.findAll('team');
+
+    // return this.store.findAll('team');
+    return null;
   },
 
   // Set model as 'teams' for route's controller
   setupController: function(controller, model) {
-    console.log('setting up controller...');
     controller.set('teams', model);
   }
-
 });
